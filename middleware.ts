2c6dd24f45a59e0230677318f0a44a63e5d39971
Teodor-Alexandru Rizon <129885407+TeodorRizon21@users.noreply.github.com>
2025-05-15 17:39:17 +0300

    dradra

--- middleware.ts
+++ middleware.ts
@@ -14,6 +14,7 @@ const isPublicRoute = createRouteMatcher([
   "/api/faculties",
   "/api/user",
   "/payment/(.*)",
+  "/api/webhooks/clerk",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
